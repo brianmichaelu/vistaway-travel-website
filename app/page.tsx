0564971eb83b9bc8be7922b34e78843397e237d0
brianmichaelu <brianmichaelu@gmail.com>
2026-05-28 15:18:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,6 @@ export default function Home() {
       <DubaiVisaForm />
       <Packages />
       <WhyChooseUs />
-      <HowItWorks />
-      <Testimonials />
       <Contact />
       <Footer />
     </main>
