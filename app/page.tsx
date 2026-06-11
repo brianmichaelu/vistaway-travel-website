de111769b329387357b2d4e947f2f0d3f41a0d4d
brianmichaelu <brianmichaelu@gmail.com>
2026-06-11 08:12:50 +0300

    Adjust padding in main component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-hidden pt-[78px]">
+    <main className="min-h-screen overflow-hidden pt-[72px]">
       <Header />
       <Hero />
       <FlightBookingForm />
